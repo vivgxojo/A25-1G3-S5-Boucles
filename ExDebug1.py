ca8f173ec42d08e935cd38bf801b984de0ac7b06
vivgxojo <maryse.mongeau@cegepoutaouais.qc.ca>
2025-09-18 11:11:12 -0400

    Ajouter les input

--- ExDebug1.py
+++ ExDebug1.py
@@ -43,4 +43,7 @@ def environnement_optimal(temp, poussiere, humidite):
 
 
 if __name__ == "__main__":
-    print(environnement_optimal(25, "faible", 40))
+    temp = float(input("Entrez la température: "))
+    poussiere = input("Entrez le niveau de poussière: ")
+    humidite = float(input("Entrez l'humidité: "))
+    print(environnement_optimal(temp, poussiere, humidite))
